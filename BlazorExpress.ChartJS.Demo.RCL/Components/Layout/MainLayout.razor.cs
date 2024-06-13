2931f398efd493753bcb496efbd278c1591aab6e
Vikram Reddy <gvreddy04@gmail.com>
2024-06-13 11:17:57 +0530

    Bar chart - Demos update

--- BlazorExpress.ChartJS.Demo.RCL/Components/Layout/MainLayout.razor.cs
+++ BlazorExpress.ChartJS.Demo.RCL/Components/Layout/MainLayout.razor.cs
@@ -6,19 +6,32 @@ public partial class MainLayout : MainLayoutBase
     {
         navItems ??= new List<NavItem>
         {
-            new (){ Id = "1", Text = "Getting Started", Href = "/getting-started", IconName = IconName.HouseDoorFill },
+            new (){ Id = "1", Text = "Overview", Href = "/overview", IconName = IconName.HouseDoorFill },
+            new (){ Id = "2", Text = "Getting Started", Href = "/getting-started", IconName = IconName.Palette2 },
 
-            new (){ Id = "2", Text = "Bar Chart", IconName = IconName.BarChartFill },
-            new (){ Id = "200", Text = "API", Href = "/charts/bar-chart", IconName = IconName.BarChartFill, ParentId = "2", Match = NavLinkMatch.All },
+            new (){ Id = "3", Text = "Bar Chart", IconName = IconName.BarChartLineFill },
+            new (){ Id = "301", Text = "Bar", Href = "/charts/bar-chart", IconName = IconName.BarChartLine, ParentId = "3", Match = NavLinkMatch.All },
+            new (){ Id = "302", Text = "Horizontal", Href = "/charts/bar-chart/horizontal", IconName = IconName.BarChartLine, ParentId = "3", Match = NavLinkMatch.All },
+            new (){ Id = "303", Text = "Stacked", Href = "/charts/bar-chart/stacked", IconName = IconName.BarChartLine, ParentId = "3", Match = NavLinkMatch.All },
+            new (){ Id = "304", Text = "Data labels", Href = "/charts/bar-chart/data-labels", IconName = IconName.BarChartLine, ParentId = "3", Match = NavLinkMatch.All },
+            new (){ Id = "305", Text = "Locale", Href = "/charts/bar-chart/locale", IconName = IconName.BarChartLine, ParentId = "3", Match = NavLinkMatch.All },
 
-            new (){ Id = "3", Text = "Doughnut Chart", IconName = IconName.CircleFill, },
-            new (){ Id = "300", Text = "API", Href = "/charts/doughnut-chart", IconName = IconName.CircleFill, ParentId = "3", Match = NavLinkMatch.All },
+            new (){ Id = "4", Text = "Doughnut Chart", IconName = IconName.CircleFill, },
+            new (){ Id = "400", Text = "Doughnut", Href = "/charts/doughnut-chart", IconName = IconName.Circle, ParentId = "4", Match = NavLinkMatch.All },
+            new (){ Id = "400", Text = "Data labels", Href = "/charts/doughnut-chart/doughnut-labels", IconName = IconName.Circle, ParentId = "4", Match = NavLinkMatch.All },
+            new (){ Id = "400", Text = "Locale", Href = "/charts/doughnut-chart/locale", IconName = IconName.Circle, ParentId = "4", Match = NavLinkMatch.All },
 
-            new (){ Id = "4", Text = "Line Chart", IconName = IconName.GraphUp },
-            new (){ Id = "400", Text = "API", Href = "/charts/line-chart", IconName = IconName.GraphUp, ParentId = "4", Match = NavLinkMatch.All },
+            new (){ Id = "5", Text = "Line Chart", IconName = IconName.GraphUpArrow },
+            new (){ Id = "500", Text = "Line", Href = "/charts/line-chart", IconName = IconName.GraphUp, ParentId = "5", Match = NavLinkMatch.All },
+            new (){ Id = "500", Text = "Data labels", Href = "/charts/line-chart/data-labels", IconName = IconName.GraphUp, ParentId = "5", Match = NavLinkMatch.All },
+            new (){ Id = "500", Text = "Tick configuration", Href = "/charts/line-chart/tick-configuration", IconName = IconName.GraphUp, ParentId = "5", Match = NavLinkMatch.All },
+            new (){ Id = "500", Text = "Locale", Href = "/charts/line-chart/locale", IconName = IconName.GraphUp, ParentId = "5", Match = NavLinkMatch.All },
 
-            new (){ Id = "5", Text = "Pie Chart", IconName = IconName.PieChartFill },
-            new (){ Id = "500", Text = "API", Href = "/charts/pie-chart", IconName = IconName.PieChartFill, ParentId = "5", Match = NavLinkMatch.All },
+            new (){ Id = "6", Text = "Pie Chart", IconName = IconName.PieChartFill },
+            new (){ Id = "600", Text = "Pie", Href = "/charts/pie-chart", IconName = IconName.PieChart, ParentId = "6", Match = NavLinkMatch.All },
+            new (){ Id = "600", Text = "Data labels", Href = "/charts/pie-chart/data-labels", IconName = IconName.PieChart, ParentId = "6", Match = NavLinkMatch.All },
+            new (){ Id = "600", Text = "Legend position", Href = "/charts/pie-chart/legend-position", IconName = IconName.PieChart, ParentId = "6", Match = NavLinkMatch.All },
+            new (){ Id = "600", Text = "Locale", Href = "/charts/pie-chart/locale", IconName = IconName.PieChart, ParentId = "6", Match = NavLinkMatch.All },
 
         };
 
---
+++ BlazorExpress.ChartJS.Demo.RCL/Components/Shared/Prerequisites.razor.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorExpress.ChartJS.Demo.RCL;
+
+public partial class Prerequisites : ComponentBase
+{
+    [Parameter]
+    public string? PageUrl { get; set; }
+}
