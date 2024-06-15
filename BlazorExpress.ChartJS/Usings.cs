fd3d780d08f479fe0cbd6a1fd8074617959a8346
Vikram Reddy <gvreddy04@gmail.com>
2024-06-15 14:42:32 +0530

    Demos and other improvements (#5)

--- BlazorExpress.ChartJS/Usings.cs
+++ BlazorExpress.ChartJS/Usings.cs
@@ -1,4 +1,5 @@
 ﻿global using Microsoft.AspNetCore.Components;
 global using Microsoft.JSInterop;
+global using System.Drawing;
 global using System.Globalization;
 global using System.Text.Json.Serialization;
