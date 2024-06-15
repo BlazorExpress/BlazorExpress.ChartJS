94ce4ecbcd8186cc5ab4cd453f8c94faa96994e3
Vikram Reddy <gvreddy04@gmail.com>
2024-06-16 00:51:03 +0530

    Bar chart Api documentation (#8)
    * Bar chart Api documentation

---
+++ BlazorExpress.ChartJS.Demo.RCL/Components/Shared/Docs/DocxTRow.razor.cs
@@ -0,0 +1,33 @@
+﻿namespace BlazorExpress.ChartJS.Demo.RCL;
+
+public partial class DocxTRow<TItem> : ComponentBase
+{
+    [Parameter]
+    public string? PropertyName { get; set; }
+
+    [Parameter]
+    public string? PropertyType { get; set; }
+
+    [Parameter]
+    public string? DefaultValue { get; set; }
+
+    [Parameter]
+    public bool Required { get; set; }
+
+    [Parameter]
+    public string? Description { get; set; }
+
+    [Parameter]
+    public string? AddedVersion { get; set; }
+
+    [CascadingParameter(Name = "DocType")]
+    public DocType DocType { get; set; } = DocType.Parameters;
+
+    [Parameter]
+    public string? MethodName { get; set; }
+
+    [Parameter]
+    public string? ReturnType { get; set; }
+
+
+}
