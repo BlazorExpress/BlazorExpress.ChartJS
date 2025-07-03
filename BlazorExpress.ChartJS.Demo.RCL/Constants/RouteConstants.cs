afc277de5feeb270fe1e370bf91249ef7dffa895
Vikram Reddy <gvreddy04@gmail.com>
2025-07-04 00:29:10 +0530

    Demos - Rotes - draft

--- BlazorExpress.ChartJS.Demo.RCL/Constants/RouteConstants.cs
+++ BlazorExpress.ChartJS.Demo.RCL/Constants/RouteConstants.cs
@@ -4,25 +4,36 @@ public static class RouteConstants
 {
     public const string Blog_Prefix = "/blog";
     public const string Docs_Prefix = "/docs";
-    public const string Demos_Prefix = "";
+    public const string Demos_Prefix = "/demos";
+
+    #region Demos
+                         
+    public const string Docs_BarChart_Route = Docs_Prefix + "/bar-chart";
+    public const string Docs_DoughnutChart_Route = Docs_Prefix + "/doughnut-chart";
+    public const string Docs_LineChart_Route = Docs_Prefix + "/line-chart";
+    public const string Docs_PieChart_Route = Docs_Prefix + "/pie-chart";
+    public const string Docs_PolarAreaChart_Route = Docs_Prefix + "/polar-area-chart";
+    public const string Docs_RadarChart_Route = Docs_Prefix + "/radar-chart";
+    public const string Docs_ScatterChart_Route = Docs_Prefix + "/scatter-chart";
+
+    #endregion
 
     #region Demos
 
     // Getting Started
-    public const string Demos_GettingStarted_Documentation = Demos_Prefix + "/getting-started";
+    public const string Demos_GettingStarted_Route = Demos_Prefix + "/getting-started";
 
-    public const string Demos_Charts_Prefix = Demos_Prefix + "/charts";
-    public const string Demos_BarChart_Documentation = Demos_Charts_Prefix + "/bar-chart";
-    public const string Demos_DoughnutChart_Documentation = Demos_Charts_Prefix + "/doughnut-chart";
-    public const string Demos_LineChart_Documentation = Demos_Charts_Prefix + "/line-chart";
-    public const string Demos_PieChart_Documentation = Demos_Charts_Prefix + "/pie-chart";
-    public const string Demos_PolarAreaChart_Documentation = Demos_Charts_Prefix + "/polar-area-chart";
-    public const string Demos_RadarChart_Documentation = Demos_Charts_Prefix + "/radar-chart";
-    public const string Demos_ScatterChart_Documentation = Demos_Charts_Prefix + "/scatter-chart";
+    public const string Demos_BarChart_Route = Demos_Prefix + "/bar-chart";
+    public const string Demos_DoughnutChart_Route = Demos_Prefix + "/doughnut-chart";
+    public const string Demos_LineChart_Route = Demos_Prefix + "/line-chart";
+    public const string Demos_PieChart_Route = Demos_Prefix + "/pie-chart";
+    public const string Demos_PolarAreaChart_Route = Demos_Prefix + "/polar-area-chart";
+    public const string Demos_RadarChart_Route = Demos_Prefix + "/radar-chart";
+    public const string Demos_ScatterChart_Route = Demos_Prefix + "/scatter-chart";
 
     // Utilities
     public const string Demos_Utils_Prefix = Demos_Prefix + "/utils";
-    public const string Demos_ColorUtils_Documentation = Demos_Utils_Prefix + "/color-utility";
+    public const string Demos_ColorUtils_Route = Demos_Utils_Prefix + "/color-utility";
 
     #endregion
 }
---
+++ BlazorExpress.ChartJS.Demo.RCL/Models/PageLink.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorExpress.ChartJS.Demo.RCL;
+
+public class PageLink
+{
+    public int Id { get; set; }
+    public IconName IconName { get; set; } = default!;
+    public string Href { get; set; } = default!;
+    public string Text { get; set; } = default!;
+    public int SortOrder { get; set; }
+    public HashSet<PageLinkCategory> Categories { get; set; } = new();
+    public PageLinkStatus Status { get; set; }
+    public bool IsActive { get; set; }
+}
