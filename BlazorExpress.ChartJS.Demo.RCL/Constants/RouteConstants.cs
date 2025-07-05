683037b4f5ce2ff1a6070a2a9a9e80477dc7719b
Vikram Reddy <gvreddy04@gmail.com>
2025-07-06 02:12:31 +0530

    Blog releated updates - draft

--- BlazorExpress.ChartJS.Demo.RCL/Constants/RouteConstants.cs
+++ BlazorExpress.ChartJS.Demo.RCL/Constants/RouteConstants.cs
@@ -7,29 +7,12 @@ public static class RouteConstants
     public const string Home_Prefix = "/";
 
     public const string Blog_Prefix = "/blog";
-    public const string Docs_Prefix = "/docs";
     public const string Demos_Prefix = "/demos";
+    public const string Docs_Prefix = "/docs";
 
-    #region Docs
+    #region Blog
 
-    // Getting Started
-    public const string Docs_GettingStarted = Docs_Prefix + "/getting-started";
-    public const string Docs_GettingStarted_Blazor_WebAssembly_NET8 = Docs_Prefix + "/blazor-webassembly-net-8";
-    public const string Docs_GettingStarted_Blazor_WebApp_NET_8_Interactive_Render_Mode_Server_Global_Location = Docs_Prefix + "/blazor-webapp-server-global-net-8";
-    public const string Docs_GettingStarted_Blazor_WebApp_NET_8_Interactive_Render_Mode_Auto_Global_Location = Docs_Prefix + "/blazor-webapp-auto-global-net-8";
-
-    // Charts
-    public const string Docs_BarChart = Docs_Prefix + "/bar-chart";
-    public const string Docs_DoughnutChart = Docs_Prefix + "/doughnut-chart";
-    public const string Docs_LineChart = Docs_Prefix + "/line-chart";
-    public const string Docs_PieChart = Docs_Prefix + "/pie-chart";
-    public const string Docs_PolarAreaChart = Docs_Prefix + "/polar-area-chart";
-    public const string Docs_RadarChart = Docs_Prefix + "/radar-chart";
-    public const string Docs_ScatterChart = Docs_Prefix + "/scatter-chart";
-
-    // Utils
-    public const string Docs_Utils_Prefix = Docs_Prefix + "/utils";
-    public const string Docs_ColorUtils = Docs_Utils_Prefix + "/color-utility";
+    public const string Blog_Home = Blog_Prefix + "/home";
 
     #endregion
 
@@ -51,6 +34,29 @@ public static class RouteConstants
     // Utils
     public const string Demos_Utils_Prefix = Demos_Prefix + "/utils";
     public const string Demos_ColorUtils = Demos_Utils_Prefix + "/color-utility";
+
+    #endregion
+
+    #region Docs
+
+    // Getting Started
+    public const string Docs_GettingStarted = Docs_Prefix + "/getting-started";
+    public const string Docs_GettingStarted_Blazor_WebAssembly_NET8 = Docs_Prefix + "/blazor-webassembly-net-8";
+    public const string Docs_GettingStarted_Blazor_WebApp_NET_8_Interactive_Render_Mode_Server_Global_Location = Docs_Prefix + "/blazor-webapp-server-global-net-8";
+    public const string Docs_GettingStarted_Blazor_WebApp_NET_8_Interactive_Render_Mode_Auto_Global_Location = Docs_Prefix + "/blazor-webapp-auto-global-net-8";
+
+    // Charts
+    public const string Docs_BarChart = Docs_Prefix + "/bar-chart";
+    public const string Docs_DoughnutChart = Docs_Prefix + "/doughnut-chart";
+    public const string Docs_LineChart = Docs_Prefix + "/line-chart";
+    public const string Docs_PieChart = Docs_Prefix + "/pie-chart";
+    public const string Docs_PolarAreaChart = Docs_Prefix + "/polar-area-chart";
+    public const string Docs_RadarChart = Docs_Prefix + "/radar-chart";
+    public const string Docs_ScatterChart = Docs_Prefix + "/scatter-chart";
+
+    // Utils
+    public const string Docs_Utils_Prefix = Docs_Prefix + "/utils";
+    public const string Docs_ColorUtils = Docs_Utils_Prefix + "/color-utility";
 
     #endregion
 }
