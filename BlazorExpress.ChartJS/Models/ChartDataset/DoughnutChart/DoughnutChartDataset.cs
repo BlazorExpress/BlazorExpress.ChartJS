b619cb39b9ee8b7c31cd25e58c04f671e2d94239
Vikram Reddy <gvreddy04@gmail.com>
2025-07-29 23:13:12 +0530

    Update chart documentation and enhance DoughnutChartDataset
    Updated documentation for BarChart, DoughnutChart, LineChart, PieChart, PolarAreaChart, RadarChart, and ScatterChart to replace specific options sections with "ChartData Properties" and "DatasetDataLabels Properties." Removed "Options Properties" sections and re-added them at the end of each file.
    
    Enhanced the `DoughnutChartDataset` class by adding and modifying several properties, including versioning attributes, default values, and descriptions for properties such as `BackgroundColor`, `BorderAlign`, `BorderColor`, and others, improving clarity and documentation quality.
    
    NOTE: This commit message is auto-generated using GitHub Copilot.

--- BlazorExpress.ChartJS/Models/ChartDataset/DoughnutChart/DoughnutChartDataset.cs
+++ BlazorExpress.ChartJS/Models/ChartDataset/DoughnutChart/DoughnutChartDataset.cs
@@ -11,10 +11,14 @@ public class DoughnutChartDataset : ChartDataset<double?>
 
     /// <summary>
     /// Arc background color.
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is 'rgba(0, 0, 0, 0.1)'.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue("rgba(0, 0, 0, 0.1)")]
+    [Description("Arc background color.")]
+    [ParameterTypeName("List<string>?")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<string>? BackgroundColor { get; set; }
 
@@ -22,177 +26,255 @@ public class DoughnutChartDataset : ChartDataset<double?>
     /// Supported values are 'center' and 'inner'.
     /// When 'center' is set, the borders of arcs next to each other will overlap. 
     /// When 'inner' is set, it is guaranteed that all borders will not overlap.
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is 'center'.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue("center")]
+    [Description("Supported values are 'center' and 'inner'. When 'center' is set, the borders of arcs next to each other will overlap. When 'inner' is set, it is guaranteed that all borders will not overlap.")]
+    [ParameterTypeName("List<string>?")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<string>? BorderAlign { get; set; } // TODO: change this to enum
 
     /// <summary>
     /// Arc border color.
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is '#fff'.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue("#fff")]
+    [Description("Arc border color.")]
+    [ParameterTypeName("List<string>?")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<string>? BorderColor { get; set; }
 
     /// <summary>
     /// Arc border length and spacing of dashes.
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is <see langword="null"/>.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(null)]
+    [Description("Arc border length and spacing of dashes.")]
+    [ParameterTypeName("List<double>?")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<double>? BorderDash { get; set; }
 
     /// <summary>
     /// Arc border offset for line dashes.
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is 0.0.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue("0.0")]
+    [Description("Arc border offset for line dashes.")]
+    //[ParameterTypeName("")]
     public double BorderDashOffset { get; set; }
 
     /// <summary>
     /// Arc border join style.
     /// Supported values are 'round', 'bevel', 'miter'.
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is <see langword="null"/>.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(null)]
+    [Description("Arc border join style. Supported values are 'round', 'bevel', 'miter'.")]
+    [ParameterTypeName("List<string>?")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<string>? BorderJoinStyle { get; set; } // TODO: change this to enum
 
     /// <summary>
     /// It is applied to all corners of the arc (outerStart, outerEnd, innerStart, innerRight).
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is 0.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(0)]
+    [Description("It is applied to all corners of the arc (outerStart, outerEnd, innerStart, innerRight).")]
+    [ParameterTypeName("List<double>?")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<double>? BorderRadius { get; set; }
 
     /// <summary>
     /// Arc border width (in pixels).
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is 2.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(2)]
+    [Description("Arc border width (in pixels).")]
+    [ParameterTypeName("List<double>?")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<double>? BorderWidth { get; set; }
 
     /// <summary>
     /// Per-dataset override for the sweep that the arcs cover.
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is <see langword="null"/>.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(null)]
+    [Description("Per-dataset override for the sweep that the arcs cover.")]
+    [ParameterTypeName("double?")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public double? Circumference { get; set; }
 
+    [AddedVersion("1.0.0")]
+    [DefaultValue("new()")]
+    [Description("")]
+    [ParameterTypeName("DoughnutChartDatasetDataLabels")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)] 
     public DoughnutChartDatasetDataLabels Datalabels { get; set; } = new(); // TODO: add the reference link
 
     /// <summary>
     /// Arc background color when hovered.
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is <see langword="null"/>.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(null)]
+    [Description("Arc background color when hovered.")]
+    [ParameterTypeName("List<string>?")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<string>? HoverBackgroundColor { get; set; }
 
     /// <summary>
     /// Arc border color when hovered.
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is <see langword="null"/>.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(null)]
+    [Description("Arc border color when hovered.")]
+    [ParameterTypeName("List<string>?")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<string>? HoverBorderColor { get; set; }
 
     /// <summary>
-    /// Arc border length and spacing of dashes when hovered. 
-    /// </summary>
-    /// <remarks>
+    /// Arc border length and spacing of dashes when hovered.
+    /// <para>
     /// Default value is <see langword="null"/>.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(null)]
+    [Description("Arc border length and spacing of dashes when hovered.")]
+    [ParameterTypeName("List<double>?")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<double>? HoverBorderDash { get; set; }
 
     /// <summary>
     /// Arc border offset for line dashes when hovered.
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is <see langword="null"/>.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(null)]
+    [Description("Arc border offset for line dashes when hovered.")]
+    [ParameterTypeName("double?")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public double? HoverBorderDashOffset { get; set; }
 
     /// <summary>
     /// Arc border join style when hovered.
     /// Supported values are 'round', 'bevel', 'miter'.
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is <see langword="null"/>.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(null)]
+    [Description("Arc border join style when hovered. Supported values are 'round', 'bevel', 'miter'.")]
+    [ParameterTypeName("List<string>?")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<string>? HoverBorderJoinStyle { get; set; } // TODO: change this to enum
 
     /// <summary>
     /// Arc border width when hovered (in pixels).
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is <see langword="null"/>.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(null)]
+    [Description("Arc border width when hovered (in pixels).")]
+    [ParameterTypeName("List<double>?")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<double>? HoverBorderWidth { get; set; }
 
     /// <summary>
     /// Arc offset when hovered (in pixels).
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is 0.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(0)]
+    [Description("Arc offset when hovered (in pixels).")]
+    [ParameterTypeName("List<double>?")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<double>? HoverOffset { get; set; }
 
     /// <summary>
     /// Arc offset (in pixels).
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is 0.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(0)]
+    [Description("Arc offset (in pixels).")]
+    [ParameterTypeName("List<double>?")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<double>? Offset { get; set; }
 
     /// <summary>
     /// Per-dataset override for the starting angle to draw arcs from.
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is <see langword="null"/>.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(null)]
+    [Description("Per-dataset override for the starting angle to draw arcs from.")]
+    [ParameterTypeName("double?")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public double? Rotation { get; set; }
 
     /// <summary>
     /// Fixed arc offset (in pixels). Similar to <see cref="Offset" /> but applies to all arcs.
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is 0.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(0)]
+    [Description("Fixed arc offset (in pixels). Similar to <code>Offset</code> but applies to all arcs.")]
     public double Spacing { get; set; }
 
     /// <summary>
     /// The relative thickness of the dataset. 
     /// Providing a value for weight will cause the pie or doughnut dataset to be drawn 
     /// with a thickness relative to the sum of all the dataset weight values.
-    /// </summary>
-    /// <remarks>
+    /// <para>
     /// Default value is 1.
-    /// </remarks>
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(1)]
+    [Description("The relative thickness of the dataset. Providing a value for weight will cause the pie or doughnut dataset to be drawn with a thickness relative to the sum of all the dataset weight values.")]
     public double Weight { get; set; } = 1;
 
     #endregion
